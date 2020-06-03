4bab19a893f84775db44c2b2fe803320447ac73a
Raymond Oung <r.oung@hapi-robo.com>
2020-06-03 22:17:37 +0900

    Translates Japanese text

--- locale/jp.js
+++ locale/jp.js
@@ -3,12 +3,55 @@ module.exports = {
   phrases: {
     "language": "日本語", 
     "login": {
-      "greetings": "Log in to your account",
+      "greetings": "アカウントにログイン",
       "email": "メールアドレス",
       "password": "パスワード",
       "login": "ログイン",
-      "signup_msg": "Don't have an account?",
-      "signup": "Sign Up"
+      "signup_msg": "アカウントをお持ちでない場合",
+      "signup": "サインアップ"
     },
+    "devices": {
+      "title": "デバイス",
+      "name": "デバイス名",
+      "serial": "シリアルナンバー",
+      "add": "追加",
+    },
+    "navbar": {
+      "profile": "プロファイル",
+      "devices": "デバイス",
+      "settings": "設定",
+      "logout": "ログアウト"
+    },
+    "profile": {
+      "title": "プロファイル",    
+      "first_name": "名",
+      "last_name": "姓",
+      "email": "メールアドレス",
+      "current_password": "現在のパスワード",
+      "new_password": "新しいパスワード",
+      "confirm_new_password": "新しいパスワードを確認",
+      "save": "保存"
+    },
+    "dashboard": {
+      "title": "ダッシュボード",    
+      "choose_location": "ロケーションを選択...",
+      "go": "進む",
+      "devices": "デバイス",
+      "home_base": "ホームベース",
+      "command": "動作"
+    },
+    "register": {
+      "title": "登録",
+      "greetings": "アカウントを作成",
+      "first_name": "名",
+      "last_name": "姓",
+      "email": "メールアドレス",
+      "password": "パスワード",
+      "create_password": "パスワードを設定",
+      "confirm_password": "パスワードを確認",
+      "register": "登録",
+      "account_msg": "既存のアカウントをお持ちの場合",
+      "login": "ログイン"
+    }
   }
 };
